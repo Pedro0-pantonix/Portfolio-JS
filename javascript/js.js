6da747fe5357e58fe20b3ad4fbf8dac8140cc7f1
Pedro0-pantonix <pedrocavalcantedarosa@gmail.com>
2023-01-04 15:54:28 -0300

    weatherapp js changes

--- javascript/js.js
+++ javascript/js.js
@@ -1,2 +1,46 @@
-//e34964ed02fa802e1ac0254c7ef561f7//
-//https://youtu.be/VS8EBgPwsSU
+// Variaveis e seleção de elementos
+
+const apiKey = "e34964ed02fa802e1ac0254c7ef561f7";
+const apiCountryURL = "https://countryflagsapi.com/png/";
+
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
+const cityElement = document.querySelector("#city");
+const tempElement = document.querySelector("#temperature span");
+const descElement = document.querySelector("#description");
+const weatherIconElement = document.querySelector("#weather-icon");
+const countryElement = document.querySelector("#country");
+const umidityElement = document.querySelector("#umidity span");
+const windElement = document.querySelector("#wind span");
+
+
+
+//Funções
+
+const getWeatherData = async(city) => {
+  const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}$lang=pt_br`;
+
+  const res = await fetch(apiWeatherURL);
+  const data = await res.json();
+
+  console.log(data);
+}
+
+
+const showWeatherData = (city) => {
+  getWeatherData(city);
+}
+
+
+
+//Eventos 
+
+
+searchBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const city = cityInput.value;
+
+  showWeatherData(city);
+});
+
